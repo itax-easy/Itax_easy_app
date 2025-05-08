3653329b92125305de43e7d1358685a09c1e7481
shoaib3474 <shoaib3974@gmail.com>
2025-05-08 17:32:11 +0500

    create investment calculator screens

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,12 @@ import 'package:itax_easy_app/views/screens/calculators/icome_tax_calculators/ca
 import 'package:itax_easy_app/views/screens/calculators/icome_tax_calculators/deprecication_calculator_screen.dart';
 import 'package:itax_easy_app/views/screens/calculators/icome_tax_calculators/tax_calculator_screen.dart';
 import 'package:itax_easy_app/views/screens/calculators/insurance%20calculators/NPS_calculator_screen.dart';
+import 'package:itax_easy_app/views/screens/calculators/investment%20calculators/CARG_calculators_screen.dart';
+import 'package:itax_easy_app/views/screens/calculators/investment%20calculators/FD_calculator_screen.dart';
+import 'package:itax_easy_app/views/screens/calculators/investment%20calculators/RD_calculator_screen.dart';
+import 'package:itax_easy_app/views/screens/calculators/investment%20calculators/SIP_calculator_screen.dart';
+import 'package:itax_easy_app/views/screens/calculators/investment%20calculators/lump_sum_calculator_screen.dart';
+import 'package:itax_easy_app/views/screens/calculators/investment%20calculators/post_office_MIS_calculator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,7 +29,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: CapitalGainCalculatorScreen(),
+      home: SIPCalculatorScreen(),
     );
   }
 }
+
+
+//Post Office MIS (Monthly Income Schedule) Calculator
+
+// Recursive Deposit Calculator
